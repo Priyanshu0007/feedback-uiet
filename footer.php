75cbcbce3033cb0f82fbccb27252dbce093f1c0f
ashivliving <ashivliving@gmail.com>
2017-04-30 00:38:41 +0530

    footer UI improved

--- footer.php
+++ footer.php
@@ -5,6 +5,6 @@
 <!--<tr><td>&nbsp;</td></tr>-->
 <tr>
 <td align="left" valign="top">&nbsp;</td>
-<td align="right"><font face="Times New Roman, Times, serif" size="-1" ><br/><font size="2" >Developed By: Ashiv Gupta & Nikhil Sardana<br/>UE148021</font></td>
+<td align="right"><font face="Times New Roman, Times, serif" size="-1" ><br/><font size="2" >Developed By: <a href="http://www.ashivgupta.me">Ashiv Gupta</a></font></td>
 </tr>
 </table>
